e4303bc2aa9aa8a97e111523e6878c5d8fc77080
Ali Eissa <aeissa@localhost.localdomain>
2016-10-02 01:40:22 -0400

    firebase app moved out of app.config

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,6 +9,14 @@
  * Main module of the application.
  */
 
+firebase.initializeApp({
+   apiKey: "AIzaSyAsfPArCOskSBxDgZSXawQo0QNyakC5PPc",
+   authDomain: "octranspo-a9250.firebaseapp.com",
+   databaseURL: "https://octranspo-a9250.firebaseio.com",
+   storageBucket: "octranspo-a9250.appspot.com",
+   messagingSenderId: "180165321438"
+ });
+
 angular
   .module('busTrackerApp', [
     'firebase',
@@ -23,13 +31,13 @@ angular
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
+        // controller: 'MainCtrl',
+        // controllerAs: 'main'
       })
       .when('/about', {
         templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
+        // controller: 'AboutCtrl',
+        // controllerAs: 'about'
       })
       .when('/routes', {
         templateUrl: 'views/routes.html',
@@ -65,7 +73,7 @@ angular
         redirectTo: '/'
       });
 
-      firebase.initializeApp(FIREBASECONFIG);
+
 
       $firebaseRefProvider.registerUrl({
         default: FIREBASECONFIG.databaseURL,
