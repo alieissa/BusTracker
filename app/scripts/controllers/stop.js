1aa748c411c1f110e49a6a2b3eac6fc2be29c353
Ali Eissa <aeissa@localhost.localdomain>
2016-10-04 03:00:21 -0400

    Bus stop trips data now relayed to stop view

--- app/scripts/controllers/stop.js
+++ app/scripts/controllers/stop.js
@@ -15,7 +15,23 @@ StopCtrl.$inject = ['$routeParams', 'stops'];
 
 function StopCtrl($routeParams, stops) {
   var vm = this;
-
+  vm.routes = [];
   vm.stopNo = $routeParams.stopNo;
-  stops.getNextTrips($routeParams.stopNo);
+
+  activate();
+
+  function activate() {
+    return getRouteSummary().then(function(routeData) {
+      console.log(routeData);
+    });
+  }
+
+  function getRouteSummary(){
+    return stops.getRouteSummary(vm.stopNo)
+      .then(function(routeSummary) {
+        vm.routes = routeSummary.Routes.Route;
+        return vm.routes;
+      })
+  }
+
 }
