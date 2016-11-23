51c2a48048fe61cf29f58b51f2181cbefd477f51
alieissa <ali.portsudan@gmail.com>
2016-11-22 22:42:23 -0500

    New module for utililities.

---
+++ app/util/util.directives.js
@@ -0,0 +1,37 @@
+
+function aeMenuBar() {
+
+    let aeMenuBar = {
+        templateUrl: './views/menu-bar.html',
+        link: link
+    };
+
+    return aeMenuBar;
+
+    function link(scope, element, attrs) {
+
+        // console.log(attrs.title);
+        scope.title = attrs.title;
+       	angular.element(document).on('click', handleTouch);
+
+        function handleTouch(event) {
+
+        	let parents = angular.element(event.target).parents('#sidenav-container');
+       		let inSidenav = parents.length;
+       		let isNavIcon = angular.element(event.target).hasClass('nav-icon');
+       		let isDisplayed = angular.element('#sidenav-container').css('display');
+
+       		// console.log(parents)
+       		if(!inSidenav && isDisplayed) {
+       			angular.element('#sidenav-container').css('display', 'none');
+       		}
+
+       		if(isNavIcon) {
+       			angular.element('#sidenav-container').css('display', 'block');
+       		}
+        }
+        console.log('Route directive link fn');
+    }
+}
+
+export {aeMenuBar};
---
+++ app/util/util.module.js
@@ -0,0 +1,6 @@
+import {aeMenuBar} from './util.directives.js';
+
+angular.module('appUtil', [])
+    .directive('aeMenuBar', aeMenuBar);
+
+export default angular.module('appUtil');
