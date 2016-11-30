fe08ffd11a0c73b38fe0bb898544714f2048505a
alieissa <ali.portsudan@gmail.com>
2016-11-30 03:39:06 -0500

    Changed database service and OC service into providers.

--- app/database/database.module.js
+++ app/database/database.module.js
@@ -5,7 +5,7 @@ import {OCService} from './OC.service.js';
 
 angular.module('dBMod', [])
 	.constant('DATABASE', 'octranspo')
-	.factory('OCService', OCService)
-	.factory('dBService', dBService);
+	.provider('OCService', OCService)
+	.provider('dBService', dBService)
 
 export default angular.module('dBMod')
